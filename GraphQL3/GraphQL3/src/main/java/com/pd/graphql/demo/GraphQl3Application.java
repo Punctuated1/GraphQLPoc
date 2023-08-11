6f2e46e53acb2e8b4ce3291ade93bcf0c1dc53d1
harry <harry@harry-Inspiron-5570>
2023-08-11 12:13:48 -0400

    initial setup

---
+++ GraphQL3/GraphQL3/src/main/java/com/pd/graphql/demo/GraphQl3Application.java
@@ -0,0 +1,13 @@
+package com.pd.graphql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQl3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQl3Application.class, args);
+	}
+
+}
---
+++ GraphQL3/GraphQL3/src/test/java/com/pd/graphql/demo/GraphQl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.graphql.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQl3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
